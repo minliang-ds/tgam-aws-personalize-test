0e9bdd2412446ce034441c590c15917ee018d09c
Michal Gorniak <mggorni@amazon.com>
2021-09-13 20:35:02 +0000

    repo name

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -5,13 +5,10 @@ region="us-east-1"
 
 set -eu
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name $CODEPIPELINE_STACK_NAME \
-        --template-body file://pipeline.yaml 
-        #--parameters ParameterKey=DirectInternetAccess,ParameterValue="Enabled" \
-        #--parameters ParameterKey=NotebookName,ParameterValue="tgam-personalize-test" \
-        #--parameters ParameterKey=VolumeSize,ParameterValue="50"  \
-        #--parameters ParameterKey=BucketName,ParameterValue="tgam-personalize-test"
-        
-        
+        --template-body file://pipeline.yaml \
+        --parameters ParameterKey=RepoName,ParameterValue="amazon_personalize_streaming_events" \
+        --parameters ParameterKey=RepoBranch,ParameterValue="development" 
+
