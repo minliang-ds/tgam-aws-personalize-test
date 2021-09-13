bbcf97ac26aa27c265dc17a7df171aea8bbe9a71
Michal Gorniak <mggorni@amazon.com>
2021-09-13 20:31:22 +0000

    update names

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -8,10 +8,10 @@ set -eu
 aws cloudformation create-stack \
         --capabilities CAPABILITY_IAM \
         --stack-name $CODEPIPELINE_STACK_NAME \
-        --template-body file://pipeline.yaml \
-        --parameters ParameterKey=DirectInternetAccess,ParameterValue="Enabled" \
-        --parameters ParameterKey=NotebookName,ParameterValue="tgam-personalize-test" \
-        --parameters ParameterKey=VolumeSize,ParameterValue="50"  \
-        --parameters ParameterKey=BucketName,ParameterValue="tgam-personalize-test"
+        --template-body file://pipeline.yaml 
+        #--parameters ParameterKey=DirectInternetAccess,ParameterValue="Enabled" \
+        #--parameters ParameterKey=NotebookName,ParameterValue="tgam-personalize-test" \
+        #--parameters ParameterKey=VolumeSize,ParameterValue="50"  \
+        #--parameters ParameterKey=BucketName,ParameterValue="tgam-personalize-test"
         
         
