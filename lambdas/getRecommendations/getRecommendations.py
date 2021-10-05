1c2ee517e0a84e56c36669c9334586be55f31594
Michal Gorniak <mggorni@amazon.com>
2021-10-05 18:33:35 +0000

    rollback putevents

--- lambdas/getRecommendations/getRecommendations.py
+++ lambdas/getRecommendations/getRecommendations.py
@@ -3,15 +3,27 @@ personalize_cli = client('personalize-runtime')
 import json
 import os
 
+region = os.environ['AWS_REGION']
+account_id = os.environ['CurretnAccountId']
+
 def handler(event, context):
     print(f"Event = {event}")
     payload = json.loads(event['body'])
     try:
+        #input validation
+        if payload.get("numResults") and type(payload.get("numResults")) != int:
+            payload["numResults"] = 25;
+            
+        elif payload.get("numResults") and type(payload.get("numResults")) > 500:
+            payload["numResults"] = 500;
+            
+            
+    
         response = personalize_cli.get_recommendations(
             campaignArn=os.environ['CAMPAIGN_ARN'],
             userId=payload['userId'],
-            # numResults=123,
-            # filterArn = 'string',
+            numResults = payload.get("numResults") ? payload.get("numResults") : 25,
+            #filterArn = payload.get("filterName") ? f'arn:aws:personalize:{region}:{account_id}:filter/{payload.get("filterName")}' : f'arn:aws:personalize:{region}:{account_id}:filter/unread',
             # context=payload['context']
             )
         print(f"RawRecommendations = {response['itemList']}")
--- lambdas/putevents/putevents.js
+++ lambdas/putevents/putevents.js
@@ -12,42 +12,85 @@ exports.handler = (event, context, callback) => {
         var payload = Buffer.from(record.kinesis.data, 'base64').toString('ascii');
         console.debug('Decoded payload:', payload);
         payload = JSON.parse(payload);
-/**
+
+        if (payload.sp_app_id == undefined || payload.sp_app_id != "theglobeandmail-website"){
+            console.debug("Skipping event: not valid sp_app_id")
+            return context.successful;
+
+        }
+
+        if (payload.content_contentId === undefined || payload.content_contentId.trim().length === 0){
+            console.debug("Skipping event: not valid content_contentId")
+            return context.successful;
+        }
+        
+        if (payload.sp_derived_tstamp === undefined || payload.sp_derived_tstamp.trim().length === 0){
+            console.debug("Skipping event: not valid sp_derived_tstamp")
+            return context.successful;
+        }
+        
+        if (payload.page_type == undefined || payload.page_type != "article"){
+            console.debug("Skipping event: not valid page_type")
+            return context.successful;
+        }
+        
+        if (payload.sp_event_name == undefined || payload.sp_event_name != "page_view"){
+            console.debug("Skipping event: not valid sp_event_name")
+            return context.successful;
+        }
+
+        if (payload.sp_domain_sessionid == undefined){
+            console.debug("Skipping event: missing sp_domain_sessionid")
+            return context.successful;
+        }
+        
+        if (payload.sp_derived_tstamp == undefined){
+            console.debug("Skipping event: missing sp_derived_tstamp")
+            return context.successful;
+        }
+        
         if ((payload.sp_user_id === undefined || payload.sp_user_id.trim().length === 0) && (payload.sp_domain_userid === undefined || payload.sp_domain_userid.trim().length === 0)){
             console.debug("Skipping event: missing sp_user_id and sp_domain_userid")
             return context.successful;
         }
-        test
-    **/
-        var eventDate = new Date(payload.PublishedDate);
         
-        var putItemsParams= {
-            'datasetArn': process.env.CONTENT_DATASET_ARN,
-            'items': [
+        var eventDate = new Date(payload.sp_derived_tstamp);
+        
+        var putEventsParams= {
+            'sessionId': payload.sp_domain_sessionid,
+            'trackingId': process.env.TRACKING_ID,
+            'userId': payload.sp_user_id ? payload.sp_user_id : payload.sp_domain_userid,
+            eventList: [
                 {
-                  'itemId': payload.ContentId,
+                  'eventId': payload.sp_evenet_id,
+                  'eventType': payload.sp_event_name, 
+                  'itemId': payload.content_contentId,
+                  'sentAt': eventDate,
+                 
                   'properties': {
-                      'ContentText': payload.ContentText,
-                      'Category': payload.Category,
-                      'Section': payload.Section,
-                      'WordCount': payload.WordCount,
-                      'Published': payload.Published,
-                      'ContentType': payload.ContentType,
-                      'CREATION_TIMESTAMP': eventDate.getTime(),
-                  }
+                      'visitor_type': payload.visitor_type,
+                      'visitor_countryCode': payload.visitor_countryCode,
+                      'device_detector_visitorPlatform': payload.device_detector_visitorPlatform,
+                      'device_detector_brandName': payload.device_detector_brandName,
+                      'device_detector_browserFamily': payload.device_detector_browserFamily,
+                      //'ContentText': null,
+                      //'Category': null,
+                      //'WordCount': null
+                  }, 
                 },
             ]
         }
-
-        console.log("THIS IS THE OBJECT = " + JSON.stringify(putItemsParams,null,3))
+        console.log("THIS IS THE OBJECT = " + JSON.stringify(putEventsParams,null,3))
         console.log("THIS IS THE SOURCE OBJECT = " + JSON.stringify(payload,null,3))
 
-        personalizeevents.putItems(putItemsParams, function (err, data) {
+        personalizeevents.putEvents(putEventsParams, function (err, data) {
           if (err) {
                 console.log(err, err.stack); // an error occurred
           }
           else{ 
                 console.log("Success: " + JSON.stringify(data, null, 2)) 
+                //console.log(data);           // successful response
+                putEventsParams['eventList'][0]['sentAt']=putEventsParams['eventList'][0]['sentAt'].toTimeString();
                 const putEventsErrResponse = {
                     statusCode: 500,
                     body: JSON.stringify(err),
@@ -55,7 +98,7 @@ exports.handler = (event, context, callback) => {
                 callback(null, putEventsErrResponse);
                 const response = {
                     statusCode: 200,
-                    body: JSON.stringify(putItemsParams),
+                    body: JSON.stringify(putEventsParams),
                 };
                 callback(null, response);
           }
