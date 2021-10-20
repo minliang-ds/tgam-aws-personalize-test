d6a94dc74ddcf1f5dd2dab68264f84d25697c728
Michal Gorniak <mggorni@amazon.com>
2021-10-20 14:34:19 +0000

    update security checks

--- mlops/update.sh
+++ mlops/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd personalize-step-functions 
-cfn-lint template.yml
+cfn-lint template.yaml
 sam validate
 sam build 
 sam deploy --stack-name tgam-personalize-mlops-test  --s3-bucket sam-dev-sophi-bucket-us-east-1  --capabilities CAPABILITY_IAM  --parameter-overrides ParameterKey=Email,ParameterValue=mlinliu@amazon.com
