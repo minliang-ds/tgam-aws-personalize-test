f93fa3d9a56248757c0885cada8452a0f35c9b3c
Michal Gorniak <mggorni@amazon.com>
2021-10-27 13:56:34 -0400

    test during deployment

--- api/update_dev.sh
+++ api/update_dev.sh
@@ -30,8 +30,13 @@ sam deploy --stack-name ${stack_name}  \
 
 api_id=`aws cloudformation describe-stacks --stack-name ${stack_name}  --region ${deploy_region} --query 'Stacks[0].Outputs' --output text | grep ^ApiId | awk {'print $2'}`
 stage_name=`aws cloudformation describe-stacks --stack-name ${stack_name}  --region ${deploy_region} --query 'Stacks[0].Outputs' --output text | grep ^StageName | awk {'print $2'}`
+api_url=`aws cloudformation describe-stacks --stack-name ${stack_name}  --region ${deploy_region} --query 'Stacks[0].Outputs' --output text | grep ^POSTRecommendationsApiGatewayInvokeURL | awk {'print $2'}`
 
 aws apigateway create-deployment \
     --region ${deploy_region} \
     --rest-api-id ${api_id} \
     --stage-name ${stage_name}
+
+echo "Test api: ${api_url}"
+curl ${api_url} -H 'Content-Type: application/json' --data-raw '{"visitor_id":"e63cdc7c-742f-4442-8a24-a1cd1f36c8b0","platform":"desktop","sub_requests":[{"widget_id":"recommended-mobile_art_morestories","last_content_ids":"TTSIR6HFKZC5FNS3MSMZN7ZS3I","limit":6,"context":"mobile_art_morestories","platform":"desktop","section":"/canada/alberta/","visitor_type":"anonymous"}]}' -v
+
