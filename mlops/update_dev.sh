2c0719f59504299fa6a7a675ee696ddf41deaceb
Michal Gorniak <mggorni@amazon.com>
2021-11-08 09:25:14 -0500

    dynamic name of s3 buckert

--- mlops/update_dev.sh
+++ mlops/update_dev.sh
@@ -28,6 +28,7 @@ sam deploy ${profile_arg} --stack-name ${stack_name}  \
   ParameterKey=Environment,ParameterValue=${env}
 
 
+
 input_bucket=`aws cloudformation describe-stacks ${profile_arg} --stack-name ${stack_name}  --region ${deploy_region} --query 'Stacks[0].Outputs' --output table | grep InputBucketName  | awk -F \| {'print $4'} | awk {'print $1'}`
 aws s3 cp s3://${input_bucket}/params.json .temp_params.json ${profile_arg}  || true
 echo "Diff params.json compare to s3://${input_bucket}/params.json "
