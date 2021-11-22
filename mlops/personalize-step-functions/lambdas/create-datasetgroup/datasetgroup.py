e2af73e3682b144b11c5d3cad95eb291e2941c0b
Lin Liu <mlinliu@amazon.com>
2021-11-22 00:03:17 -0500

    adding version suffix to dataset group names

--- mlops/personalize-step-functions/lambdas/create-datasetgroup/datasetgroup.py
+++ mlops/personalize-step-functions/lambdas/create-datasetgroup/datasetgroup.py
@@ -2,6 +2,7 @@ from os import environ
 import actions
 from loader import Loader
 import ssm_parameters
+import re
 
 
 ARN = 'arn:aws:personalize:{region}:{account}:dataset-group/{name}'
@@ -9,11 +10,37 @@ LOADER = Loader()
 
 
 def lambda_handler(event, context):
-    datasetGroupArn = ARN.format(
-        account=LOADER.account_id,
-        name=event['datasetGroup']['name'],
-        region=environ['AWS_REGION']
-    )
+
+    root = event['datasetGroup']['name']
+    l = len(root)
+
+    response = LOADER.personalize_cli.list_dataset_groups()
+
+    datasetGroupNames = []
+    for datasetgroup in response['datasetGroups']:
+        if datasetgroup['name'].startswith(root):
+            datasetGroupNames.append(datasetgroup['name'])
+    
+    if len(datasetGroupNames) == 0:
+        datasetGroupArn = ARN.format(
+            account=LOADER.account_id,
+            name=event['datasetGroup']['name'] + '1',
+            region=environ['AWS_REGION']
+        )
+    else:
+        try:
+            versions = []
+            for datasetGroupName in datasetGroupNames:
+                versions.append(int(datasetGroupName[l:]))
+            datasetGroupArn = ARN.format(
+                account=LOADER.account_id,
+                name=event['datasetGroup']['name'] + str(max(versions) + 1),
+                region=environ['AWS_REGION']
+            )
+        except Exception as e:
+            LOADER.logger.error(f'Unexpected dataset group name {e}')
+            raise e
+
     try:
         status = LOADER.personalize_cli.describe_dataset_group(
             datasetGroupArn=datasetGroupArn
