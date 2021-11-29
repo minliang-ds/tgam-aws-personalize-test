786a3bbcf99df43e7e11a16d514993ea80b4fe7f
Michal Gorniak <mggorni@amazon.com>
2021-11-29 09:33:09 -0500

    print not printf

--- api/api-lambdas/putContent/putContent.py
+++ api/api-lambdas/putContent/putContent.py
@@ -73,12 +73,12 @@ def handler(event, context, metrics):
 
 
         if (payload.get('ContentId') is None or payload.get('Category') is None):
-            printf(f"Skipping content: invalid ContentId: {payload.get('Published')} or Category: {payload.get('Category')}")
+            print(f"Skipping content: invalid ContentId: {payload.get('Published')} or Category: {payload.get('Category')}")
             skip_events += 1;
             continue
 
         if (payload.get('Published') is not True):
-            printf(f"Skipping content: invalid Published {payload.get('Published')}")
+            print(f"Skipping content: invalid Published {payload.get('Published')}")
             skip_events += 1;
             continue
 
